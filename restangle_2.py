8b478c9366f2044f7d01a04c6ff74a93f8527801
Eka7yanova <95160781+Eka7yanova@users.noreply.github.com>
2022-03-04 18:57:50 +0300

    Add files via upload

---
+++ restangle_2.py
@@ -0,0 +1,28 @@
+from restangle import Restangle, Square, Circle
+
+rect_1 = Restangle(3,4)
+rect_2 = Restangle(12,5)
+
+print(rect_1.get_area())
+print(rect_2.get_area())
+
+square_1 = Square(5)
+square_2 = Square(10)
+
+print(square_1.get_area_square(),
+      square_2.get_area_square())
+
+figures = [rect_1,rect_2,square_1,square_2]
+for figure in figures:
+    if isinstance(figure,Square):
+        print(figure.get_area_square())
+    else:
+        print(figure.get_area())
+
+from math import pi
+
+circle_1 = Circle(pi,5)
+circle_2 = Circle(pi,8)
+
+print(circle_1.get_area_circle(),
+      circle_2.get_area_circle())
