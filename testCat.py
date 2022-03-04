9fa0a845bc8306f4eae923390322cd18c0164495
Eka7yanova <95160781+Eka7yanova@users.noreply.github.com>
2022-03-04 12:38:36 +0300

    Add files via upload

---
+++ testCat.py
@@ -0,0 +1,15 @@
+from cat import Cat
+
+cat_1 = Cat("Baron","man",2)
+cat_2 = Cat("Sem","man",2)
+
+print("name=", cat_1.name)
+print("gender=", cat_1.gender)
+print("age=", cat_1.age)
+print("   /\ /\    /\ /\  ")
+print("  ( 0.0 )  ( 0.0 )   ")
+print("    =u=       U    ")
+print("name=", cat_2.name)
+print("gender=", cat_2.gender)
+print("age=", cat_2.age)
+
